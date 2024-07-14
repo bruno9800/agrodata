1138daf1412cd067945aeaa5c10a1cdda8720946
Bruno <bruno9800m@gmail.com>
2024-07-14 17:33:26 -0300

    configure package ui with storybook

---
+++ packages/ui/lib/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentPropsWithoutRef } from "react";
+
+type ButtonProps = ComponentPropsWithoutRef<"button">;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, ...props }, forwardRef) => {
+    return (
+      <button
+        {...props}
+        ref={forwardRef}
+        className="px-4 py-2 rounded bg-green-400 text-white hover:bg-green-500"
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default { Button };
