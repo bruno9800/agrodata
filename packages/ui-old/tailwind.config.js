1aaafe0d1ca6f653c0544ff576613883711a9b31
Bruno <bruno9800m@gmail.com>
2024-07-15 00:02:46 -0300

    remove old folder ui

--- packages/ui-old/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["/**/*.{ts,tsx}", "./globals.css"],
-  darkMode: ["class", '[data-mode="dark"]'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
